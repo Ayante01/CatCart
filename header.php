1c8aceb01874c0c20fe2cd774a47d848f121c164
Olman Castro <ayante01@gmail.com>
2021-06-02 18:13:23 -0600

    changemeta

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="Free shipping premium cat items. Get the best quality products to take care of the health and comfy of your cat. Enjoy the lower prices of Costa Rica market."/>
     <title><?php bloginfo('name'); ?></title>
+    <meta name="description" content="Free shipping premium cat items. Get the best quality products to take care of the health and comfy of your cat. Enjoy the lower prices of Costa Rica market."/>
     <?php wp_head(); ?>
   </head>
 
