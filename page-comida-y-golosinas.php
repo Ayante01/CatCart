d7892867c150258b1d246d16340615b3045057aa
Olman Castro <ayante01@gmail.com>
2021-07-11 05:13:36 -0600

    comidaygolosinas

--- page-comida-y-golosinas.php
+++ page-comida-y-golosinas.php
@@ -3,13 +3,13 @@
 <section class="f-t-container container-fluid d-flex justify-content-between">
   <div class="f-t-col d-flex justify-content-between flex-column">
     <div class="d-flex justify-content-between">
-      <h2 class="col-1 h2-f-t">Food and Treats</h2>
+      <h2 class="col-1 h2-f-t">Comida y golosinas</h2>
       <hr class="orange-line">
     </div>
       <a class="f-t-a hvr-shrink" href="https://dev-catcartshop.pantheonsite.io/food/">
         <div class="img-fluid f-t f-t-left food">
           <div class="f-t-text d-flex justify-content-end">
-            <p class="text-left">Food</p>
+            <p class="text-left">Comida</p>
           </div>
         </div>
       </a>
@@ -17,7 +17,7 @@
       <a class="f-t-col hvr-shrink" href="https://dev-catcartshop.pantheonsite.io/treats-2/">
         <div class="img-fluid f-t f-t-right treats">
           <div class="f-t-text d-flex">
-              <p class="text-right">Treats</p>
+              <p class="text-right">Golosinas</p>
           </div>
         </div>
       </a>
